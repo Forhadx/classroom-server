ff7770398cbc93f7899f9a03780da7a09080ecb8
forhadx <forhadsh1@gmail.com>
2022-04-01 11:00:43 +0600

    avoid env

--- util/database.js
+++ util/database.js
@@ -2,25 +2,26 @@ const Sequelize = require("sequelize");
 
 // console.log(process.env);
 
-const sequelizeDb = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.HOST,
-  }
-);
-
 // const sequelizeDb = new Sequelize(
-//   "bqlfrop78volwwppwwbm",
-//   "uddq7uuismiwiirc",
-//   "xObNRGQg5YLxRSMzxYCr",
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
 //   {
-//     dialect: "mysql",
+//     dialect: process.env.DB_DIALECT,
 //     port: 3306,
-//     host: "bqlfrop78volwwppwwbm-mysql.services.clever-cloud.com",
+//     host: process.env.HOST,
 //   }
 // );
+
+const sequelizeDb = new Sequelize(
+  "bqlfrop78volwwppwwbm",
+  "uddq7uuismiwiirc",
+  "xObNRGQg5YLxRSMzxYCr",
+  {
+    dialect: "mysql",
+    port: 3306,
+    host: "bqlfrop78volwwppwwbm-mysql.services.clever-cloud.com",
+  }
+);
 
 module.exports = sequelizeDb;
